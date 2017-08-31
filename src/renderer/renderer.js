7aa7fc8ea1279b8f89328d42e0f9710c3674f04b
Felix Rieseberg <felix@felixrieseberg.com>
2017-08-30 19:22:37 -0700

    A native context menu

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,12 +1,45 @@
 const loadMonaco = require('monaco-loader')
+const { remote } = require('electron')
+const { Menu, app } = remote
 
 loadMonaco().then((monaco) => {
   const element = document.querySelector('#container')
   const options = {
     language: 'javascript',
     theme: 'vs-dark',
-    automaticLayout: true
+    automaticLayout: true,
+    // We'll make our own
+    contextmenu: false
   }
 
   const editor = monaco.editor.create(element, options)
+
+  // Context menu
+  const template = [
+    { role: 'cut' },
+    { role: 'copy' },
+    { role: 'paste' },
+    {
+      label: 'Minimize Window',
+      click (item, browserWindow, event) {
+        browserWindow.minimize()
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit',
+      click () {
+        app.quit()
+      }
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+
+  window.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    menu.popup(remote.getCurrentWindow())
+  }, false)
 })
