c199638aff010b6c596e13184e47466926bbc7db
Felix Rieseberg <felix@felixrieseberg.com>
2017-08-30 18:49:44 -0700

    An editor is born

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+const loadMonaco = require('monaco-loader')
+
+loadMonaco().then((monaco) => {
+  const element = document.querySelector('#container')
+  const options = {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  }
+
+  const editor = monaco.editor.create(element, options)
+})
