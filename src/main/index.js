bb5708011d66e87a1d66e9ca73420e4f3b8b0d78
Felix Rieseberg <felix@felixrieseberg.com>
2017-08-30 18:44:02 -0700

    A first BrowserWindow (with contents)

--- src/main/index.js
+++ src/main/index.js
@@ -4,5 +4,17 @@ let mainWindow
 
 app.on('ready', () => {
   console.log('Application ready!')
-  mainWindow = new BrowserWindow()
+
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 1000,
+    height: 800
+  })
+
+  mainWindow.webContents.on('dom-ready', () => {
+    // The window has loaded its contents
+    mainWindow.show()
+  })
+
+  mainWindow.loadURL(`file://${__dirname}/../renderer/index.html`)
 })
