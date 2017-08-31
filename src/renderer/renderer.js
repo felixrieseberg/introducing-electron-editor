73d3f716d9ab3700a04d0dafae6447845eded931
Felix Rieseberg <felix@felixrieseberg.com>
2017-08-31 09:05:01 -0700

    Sending a window.Notification

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,5 +1,15 @@
 const loadMonaco = require('monaco-loader')
 
+function sendNotification (editor) {
+  const notification = new window.Notification('Hello!', {
+    body: 'Hello from your notification system'
+  })
+
+  notification.onclick = () => {
+    editor.setValue('Notification clicked!')
+  }
+}
+
 loadMonaco().then((monaco) => {
   const element = document.querySelector('#container')
   const options = {
@@ -9,4 +19,15 @@ loadMonaco().then((monaco) => {
   }
 
   const editor = monaco.editor.create(element, options)
+
+  // Send a pointless little notification
+  editor.addAction({
+    id: 'send-notifications',
+    label: 'Send notification...',
+    contextMenuGroupId: 'navigation',
+    contextMenuOrder: 1,
+    precondition: null,
+    keybindingContext: null,
+    run: (editor) => sendNotification(editor)
+  })
 })
