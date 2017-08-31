6a368a2c53f2150e85b3f9da72b4e0d0a568ad55
Felix Rieseberg <felix@felixrieseberg.com>
2017-08-30 18:55:04 -0700

    Graceful window showing using backgroundColor

--- src/main/index.js
+++ src/main/index.js
@@ -6,14 +6,9 @@ app.on('ready', () => {
   console.log('Application ready!')
 
   mainWindow = new BrowserWindow({
-    show: false,
     width: 1000,
-    height: 800
-  })
-
-  mainWindow.webContents.on('dom-ready', () => {
-    // The window has loaded its contents
-    mainWindow.show()
+    height: 800,
+    backgroundColor: '#1e1e1e'
   })
 
   mainWindow.loadURL(`file://${__dirname}/../renderer/index.html`)
