623da1489b915c887ed07deab973279a415c3259
Felix Rieseberg <felix@felixrieseberg.com>
2017-09-27 13:25:28 -0400

    First editor

--- renderer.js
+++ renderer.js
@@ -1,1 +1,10 @@
 const loader = require('monaco-loader')
+
+loader().then((monaco) => {
+  const div = document.querySelector('#container')
+  let editor = monaco.editor.create(div, {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
+})
