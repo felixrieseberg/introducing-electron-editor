8c040e558ba9a182115c43d41020f1cec6e0725f
Felix Rieseberg <felix@felixrieseberg.com>
2017-09-27 14:53:55 -0400

    Save file dialog

--- renderer.js
+++ renderer.js
@@ -1,9 +1,13 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer, remote } = require('electron')
 const fs = require('fs')
 const loader = require('monaco-loader')
 
+const { dialog } = remote
+const browserWindow = remote.getCurrentWindow()
+
 // This is the file path for the currently open file
 let filePath
+let changed = false
 
 loader().then((monaco) => {
   const div = document.querySelector('#container')
@@ -13,18 +17,7 @@ loader().then((monaco) => {
     automaticLayout: true
   })
 
-  ipcRenderer.on('open-file', (sender, url = '') => {
-    console.log(url)
-    filePath = url
-
-    const filedata = fs.readFileSync(url, 'utf-8')
-    console.log(filedata)
-
-    const model = monaco.editor.createModel(filedata, 'javascript')
-    editor.setModel(model)
-  })
-
-  ipcRenderer.on('save-file', () => {
+  function saveFile(saveFilePath) {
     const model = editor.getModel()
     let data = ''
 
@@ -33,7 +26,42 @@ loader().then((monaco) => {
       data += line.text + model._EOL;
     });
 
-    fs.writeFileSync(filePath, data, 'utf-8')
+    fs.writeFileSync(saveFilePath, data, 'utf-8')
+  }
+
+  ipcRenderer.on('open-file', (sender, url = '') => {
+    console.log(url)
+    filePath = url
+    changed = false
+
+    const filedata = fs.readFileSync(url, 'utf-8')
+    console.log(filedata)
+
+    const model = monaco.editor.createModel(filedata, 'javascript')
+    const changeListener = model.onDidChangeContent(() => {
+      changed = true
+      changeListener.dispose()
+    })
+    editor.setModel(model)
+  })
+
+  ipcRenderer.on('save-file', () => {
+    dialog.showMessageBox(browserWindow, {
+      type: 'question',
+      title: 'Save?',
+      message: 'Do you really want to save?',
+      buttons: ['Yes', 'No']
+    }, (response) => {
+      if (response === 0 && !filePath) {
+        dialog.showSaveDialog(browserWindow, {}, (userPath) => {
+          saveFile(userPath)
+        })
+      } else if (response === 0) {
+        saveFile(filePath)
+      } else {
+        console.log('Not saving!')
+      }
+    })
   })
 })
 
