0af1808674adeedef589e8218098d9ed7f360987
Felix Rieseberg <felix@felixrieseberg.com>
2017-10-23 10:00:05 -0700

    :package: First window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello!')
+// const app = require('electron').app
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+app.on('ready', () => {
+  console.log('Application is ready!')
+
+  mainWindow = new BrowserWindow()
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
