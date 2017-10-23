ea84bb2746ad413e56b91252ac23726ea5b9889a
Felix Rieseberg <felix@felixrieseberg.com>
2017-10-23 11:06:33 -0700

    :wrench: Open file

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,37 @@
+const fs = require('fs')
+const path = require('path')
 const { remote, ipcRenderer } = require('electron')
 const loader = require('monaco-loader')
 
 const rendererWindow = remote.getCurrentWindow()
+let monaco = null
+let editor = null
 
-loader().then((monaco) => {
+async function openFile(filePath) {
+  const extension = path.extname(filePath)
+  const filetypes = {
+    '.js': 'javascript',
+    '.css': 'css'
+  }
+  const filetype = filetypes[extension] || 'text'
+
+  if (monaco && editor) {
+    try {
+      const filedata = fs.readFileSync(filePath, 'utf-8')
+      const model = monaco.editor.createModel(filedata, filetype)
+
+      editor.setModel(model)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+loader().then((_monaco) => {
   const div = document.querySelector('#container')
-  const editor = monaco.editor.create(div, {
+
+  monaco = _monaco
+  editor = monaco.editor.create(div, {
     language: 'javascript',
     theme: 'vs-dark',
     automaticLayout: true
@@ -13,7 +39,5 @@ loader().then((monaco) => {
 
   rendererWindow.show()
 
-  ipcRenderer.on('open-file', (event, url) => {
-    console.log(url)
-  })
+  ipcRenderer.on('open-file', (_e, url) => openFile(url))
  })
