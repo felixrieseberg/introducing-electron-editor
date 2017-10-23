9d6b903e8ed4ff39ade32016a98a97a641e6c2a8
Felix Rieseberg <felix@felixrieseberg.com>
2017-10-23 10:10:18 -0700

    :wrench: Path.join for appUrl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // const app = require('electron').app
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 let mainWindow
 
 app.on('ready', () => {
   console.log('Application is ready!')
 
+  const appUrl = path.join(__dirname, 'index.html')
+
   mainWindow = new BrowserWindow()
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${appUrl}`)
 })
